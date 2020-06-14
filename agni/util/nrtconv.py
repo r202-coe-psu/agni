32bff409728cef07c7d76daade19fea204289e93
ssmnp <mukoch@live.com>
2020-06-14 17:19:56 +0700

    reduce converter to functions

--- agni/util/nrtconv.py
+++ agni/util/nrtconv.py
@@ -4,60 +4,33 @@ import copy
 
 PRUNE_PROPS = ['latitude', 'longitude', 'acq_date', 'acq_time']
 
-class NRTPoint:
-    def __init__(self, nrt):
-        self.lat = nrt['latitude']
-        self.lon = nrt['longitude']
 
-        props = {k: v for k, v in nrt.items()}
-        props['acq_time_us'] = nrt['acq_time']
+def make_geojson_point(nrt):
+    lat = nrt['latitude']
+    lon = nrt['longitude']
 
-        for prop in PRUNE_PROPS:
-            if prop in props:
-                del props[prop]
+    props = {k: v for k, v in nrt.items() if k not in PRUNE_PROPS}
+    props['acq_time_us'] = nrt['acq_time']
 
-        self.properties = props
-
-    def to_dict(self):
-        ret = {
-            'type': 'Feature',
-            'properties': self.properties,
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [self.lon, self.lat]
-            }
+    ret = {
+        'type': 'Feature',
+        'properties': props,
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [lon, lat]
         }
-        return ret
-
-    def __str__(self):
-        return str(self.to_dict())
-
-
-class NRTPointCollection:
-    def __init__(self, nrt_points):
-        self.points = list(NRTPoint(p).to_dict() for p in nrt_points)
-
-    def to_dict(self):
-        ret = {
-            'type': 'FeatureCollection',
-            'features': self.points
-        }
-        return ret
-
-    def __str__(self):
-        return str(self.to_dict())
+    }
+    return ret
 
 
 def to_geojson(nrt_points):
-    # this uses raw list nrt points
     points = list(
-        NRTPoint(p).to_dict()
+        make_geojson_point(p) 
         for p in nrt_points
     )
 
-    ret_geojson = {
+    ret = {
         'type': 'FeatureCollection',
         'features': points
     }
-
-    return ret_geojson
+    return ret
