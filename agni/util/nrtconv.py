fad1638b893006576330b0180d8e7b2806b69a1f
ssmnp <mukoch@live.com>
2020-07-27 13:56:50 +0700

    refactor influxdb converter funcs

--- agni/util/nrtconv.py
+++ agni/util/nrtconv.py
@@ -4,7 +4,6 @@ import copy
 
 PRUNE_PROPS = ['latitude', 'longitude', 'acq_date', 'acq_time']
 
-
 def make_geojson_point(nrt):
     lat = nrt['latitude']
     lon = nrt['longitude']
@@ -35,9 +34,28 @@ def to_geojson(nrt_points):
     }
     return ret
 
-def to_influx_point(nrt_point, tags=None, measurement=None, skip=None):
-    if measurement is None:
-        measurement = 'hotspot'
+def to_influx_json(nrt_point, measure, timekey, tags=None, skip=None):
+    """ reshape NRT data to json compatible for InfluxDB usage
+
+        Args:
+            nrt (dict):
+                NRT data point as dict
+            measure (str):
+                InfluxDB measure name
+            timekey (str):
+                name of key in dict which contains timestamp
+                as unix epoch microseconds
+            tags (list of str):
+                list of keys within dict which should be made into tags
+                instead of fields
+            skip (list of str):
+                list of keys for values to skip processing
+
+        Return:
+            point (dict):
+                data point compatible with InfluxDB python client
+    """
+
     if tags is None:
         tags = []
     if skip is None:
@@ -47,11 +65,11 @@ def to_influx_point(nrt_point, tags=None, measurement=None, skip=None):
         'time': 0,
         'tags': {},
         'fields': {},
-        'measurement': measurement,
+        'measurement': measure,
     }
 
     for k, v in nrt_point.items():
-        if k in skip:
+        if k in skip or k == timekey:
             continue
 
         if k in tags:
@@ -65,3 +83,26 @@ def to_influx_point(nrt_point, tags=None, measurement=None, skip=None):
     influx_point['time'] = int(nrt_point['acq_time'])
 
     return influx_point
+
+def to_influx_line(nrt, measure, timekey, tags=None, skip=None):
+    point = to_influx_json(nrt, measure, timekey, tags, skip)
+
+    # formatting them into a line protocol format
+    fields_out = (
+        '='.join([str(k), str(v)])
+        for k, v in point['fields'].items()
+    )
+
+    tags_out = (
+        '='.join([str(k), str(v)])
+        for k, v in point['tags'].items()
+    )
+
+    lineprot_str = "{measure},{tags} {fields} {time}".format(
+        measure=measure,
+        tags = ','.join(tags_out),
+        fields=','.join(fields_out),
+        time=str(point['time'])
+    )
+
+    return lineprot_str
