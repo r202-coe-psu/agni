e97c93dc94f8b79ffd50da390f6b3d3ba268cd90
ssmnp <mukoch@live.com>
2021-01-22 18:03:51 +0700

    timedelta formatter

--- agni/util/timefmt.py
+++ agni/util/timefmt.py
@@ -2,7 +2,33 @@ import datetime
 import ciso8601
 
 EPOCH_START = datetime.datetime(1970,1,1)
-def parse_timestamp_us(ts):
+_DELTA_ZERO = datetime.timedelta(0)
+
+def parse_timestamp_us(ts: int):
     us = datetime.timedelta(microseconds=ts)
     return EPOCH_START + us
+
+def format_delta(delta: datetime.timedelta):
+    sign = ''
+    if delta < _DELTA_ZERO:
+        sign = '-'
+        delta = -delta
+
+    mm, ss = divmod(delta.seconds, 60)
+    hh, mm = divmod(mm, 60)
+    dd = delta.days
+    tl = [sign]
+    o = [
+        (dd, 'd'),
+        (hh, 'h'),
+        (mm, 'm'),
+        (ss, 's')
+    ]
+
+    for val, label in o:
+        if abs(val) > 0:
+            out = "{:d}{}".format(val, label)
+            tl.append(out)
+
+    return ''.join(tl)
 
