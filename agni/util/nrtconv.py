d19a1d382ea992ef9cc8cf5e1e51ac1737458661
ssmnp <mukoch@live.com>
2021-01-27 21:39:41 +0700

    use geojson lib for making pieces

--- agni/util/nrtconv.py
+++ agni/util/nrtconv.py
@@ -1,32 +1,24 @@
+import geojson
+
 PRUNE_PROPS = ['latitude', 'longitude', 'acq_date', 'acq_time']
 
-def make_geojson_point(nrt):
+def point_geojson(nrt):
     lat = nrt['latitude']
     lon = nrt['longitude']
 
     props = {k: v for k, v in nrt.items() if k not in PRUNE_PROPS}
-    #props['acq_time_us'] = nrt['acq_time']
 
-    ret = {
-        'type': 'Feature',
-        'properties': props,
-        'geometry': {
-            'type': 'Point',
-            'coordinates': [lon, lat]
-        }
-    }
+    point = geojson.Point(coordinates=(lon, lat))
+    ret = geojson.Feature(geometry=point, properties=props)
     return ret
 
 def to_geojson(nrt_points):
     points = list(
-        make_geojson_point(p) 
+        point_geojson(p)
         for p in nrt_points
     )
 
-    ret = {
-        'type': 'FeatureCollection',
-        'features': points
-    }
+    ret = geojson.FeatureCollection(points)
     return ret
 
 def to_influx_json(
