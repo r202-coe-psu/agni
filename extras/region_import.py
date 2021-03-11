09623aee69b7c86e682723781542f192cc56551d
ssmnp <mukoch@live.com>
2021-03-11 16:37:57 +0700

    fix bname extract

--- extras/region_import.py
+++ extras/region_import.py
@@ -13,13 +13,15 @@ me.connect(DBNAME)
 # assumes FeatureCollection -> Features[0] -> MultiPolygon
 with open(GJFILE, 'r') as f:
     name = os.path.basename(f.name)
-    bname = os.path.splitext(name)
+    bname = os.path.splitext(name)[0]
 
     gj = geojson.load(f)
     gjf = gj.features[0]
-    gjr = region.Region(name=bname)
-    gjr.human_name = gjf.properties['name']
+
+    gjr = region.Region()
     gjr.populate_feature(gjf)
+    gjr.name = bname
+    gjr.human_name = gjf.properties['name']
 
     rlist = region.Region.objects(name=bname)
     if len(rlist) > 0:
