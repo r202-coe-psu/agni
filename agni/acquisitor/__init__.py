52e0370f06d21446a8ec68605cd6072f3c48f2a8
asdfzdfj <nonemchome@hotmail.com>
2021-03-15 01:22:38 +0700

    demo data injector for notification testing

--- agni/acquisitor/__init__.py
+++ agni/acquisitor/__init__.py
@@ -11,6 +11,8 @@ from ..util import timefmt
 import logging
 logger = logging.getLogger(__name__)
 
+DEMO_URL = "https://t.coe.psu.ac.th/~6010110106/agni-demo/hotspot-inject.txt"
+
 class Server:
     SLEEP_SHORT = datetime.timedelta(seconds=10)
     SLEEP_LONG = datetime.timedelta(minutes=20)
@@ -18,6 +20,7 @@ class Server:
     def __init__(self, settings):
         self.settings = settings
         self.running = False
+        self.demo = False
 
         logging.basicConfig(
             level=logging.DEBUG,
@@ -30,18 +33,38 @@ class Server:
 
         self.fetcher = Fetcher(settings, self.database)
 
+        demo_mode = settings.get('FETCHER_DEMO_MODE')
+        if demo_mode is not None and demo_mode == 'yes':
+            logger.debug(r'//////// DEMO MODE ENABLED ////////')
+            self.demo = True
+
         self.notify_queue = queue.Queue()
         self.out_queues = [self.notify_queue]
 
         self.notifyd = NotifierDaemon(settings, in_queue=self.notify_queue)
         self.notifyd.start()
 
+    def demo_injector(self, base_data=None):
+        logger.debug(r'//////// DEMO INJECTOR ACTIVE ////////')
+        import requests
+        from pandas import concat, DataFrame
+        from .fetch_nrt import process_csv_pandas
+
+        req = requests.get(DEMO_URL)
+        if req.ok:
+            demo_data = process_csv_pandas(req.text)
+            hotspots = concat([base_data, demo_data], join='inner')
+            return hotspots
+        return DataFrame()
+        
     def run(self):
         self.database.wait_server()
         self.running = True
         while self.running:
             try:
                 new_data = self.fetcher.update_data(write=True)
+                if self.demo:
+                    new_data = self.demo_injector(new_data)
                 if len(new_data) > 0:
                     for q in self.out_queues:
                         q.put(new_data)
