2f104a1e2bc92fd8f9dd2622a450f06d07a8c43d
ssmnp <mulock@live.com>
2021-03-17 17:55:55 +0700

    confidence coerce/rename in data archive import

--- extras/data_migrator.py
+++ extras/data_migrator.py
@@ -2,6 +2,9 @@ import os
 from os import listdir
 from os.path import isfile, join
 
+import pandas as pd
+from pandas.api.types import is_numeric_dtype, is_string_dtype
+
 from agni.acquisitor import fetch_nrt
 from agni.database import HotspotDatabase
 
@@ -23,7 +26,21 @@ for filename in csv_file:
     with open(os.path.join(FILEPATH, filename), 'r') as f:
         text = f.read()
         df = fetch_nrt.process_csv_pandas(text)
+        confidence = df['confidence']
+        if is_numeric_dtype(confidence):
+            print('confidence is numerical, renaming keys..')
+            df['confidence_percent'] = confidence
+            del df['confidence']
+            print(df.columns)
+        elif is_string_dtype(confidence):
+            print('confidence is categorical, mapping names...')
+            df['confidence'] = confidence.map({
+                'l': 'low',
+                'n': 'nominal',
+                'h': 'high'                
+            })
+            print(df['confidence'].unique())
         print("writing {} entries from {} ...".format(len(df), filename))
-        db.write(df, measure='hotspots', precision=None, batch_size=10000)
+        db.write(df, measure='hotspots', precision=None, batch_size=5000)
         print("{} import done".format(filename))
         
